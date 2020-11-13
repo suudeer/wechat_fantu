12d6b5106e5e9bbcccae09969ab7dd38ac4f9cf4
unknown <1400273767@qq.com>
2020-11-13 18:42:33 +0800

    13

--- funto/pages/huahua/huahua.js
+++ funto/pages/huahua/huahua.js
@@ -5,8 +5,39 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    click: false, //是否显示弹窗内容
+    option: false, //显示弹窗或关闭弹窗的操作动画
   },
+ 
+  // 用户点击显示弹窗
+  clickPup: function() {
+    let _that = this;
+    if (!_that.data.click) {
+      _that.setData({
+        click: true,
+      })
+    }
+ 
+    if (_that.data.option) {
+      _that.setData({
+        option: false,
+      })
+ 
+      // 关闭显示弹窗动画的内容，不设置的话会出现：点击任何地方都会出现弹窗，就不是指定位置点击出现弹窗了
+      setTimeout(() => {
+        _that.setData({
+          click: false,
+        })
+      }, 500)
+ 
+ 
+    } else {
+      _that.setData({
+        option: true
+      })
+    }
+  },
+ 
 
   /**
    * 生命周期函数--监听页面加载
